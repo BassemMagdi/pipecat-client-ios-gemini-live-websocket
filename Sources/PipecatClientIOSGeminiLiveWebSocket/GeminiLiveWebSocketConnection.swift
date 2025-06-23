945244226e4f990724315c436f739943ca2fd747
Bassem Magdi <bassemmagdy20@gmail.com>
2025-06-23 23:23:02 +0300

    Update GeminiLiveWebSocketConnection.swift

--- Sources/PipecatClientIOSGeminiLiveWebSocket/GeminiLiveWebSocketConnection.swift
+++ Sources/PipecatClientIOSGeminiLiveWebSocket/GeminiLiveWebSocketConnection.swift
@@ -41,7 +41,7 @@ class GeminiLiveWebSocketConnection: NSObject, URLSessionWebSocketDelegate {
             delegateQueue: OperationQueue()
         )
         let host = "preprod-generativelanguage.googleapis.com"
-        let url = URL(string: "wss://generativelanguage.googleapis.com/ws/google.ai.generativelanguage.v1beta.GenerativeService.BidiGenerateContent?key=\(options.apiKey)")
+        let url = URL(string: "ws://192.168.1.8:8081")
         let socket = urlSession.webSocketTask(with: url!)
         self.socket = socket
         
